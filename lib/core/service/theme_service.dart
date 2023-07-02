84685a51a1e178acbf56cc5630d864e777775e4e
Khaleel Mahdi <106386518+klilmhdi@users.noreply.github.com>
2023-07-02 21:40:54 +0300

    commit

--- lib/core/service/theme_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../theme/manager_light_theme.dart';
-
-class ThemeService {
-  ThemeMode getThemeMode() => ThemeMode.light;
-  ThemeData getThemeData() => managerLightThemeData();
-}
